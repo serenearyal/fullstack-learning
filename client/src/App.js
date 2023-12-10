92c93f4782e87eb9e0309909d4a3bbd1d3679329
Serene Aryal <serene.aryal24@gmail.com>
2023-12-10 14:09:13 -0600

    get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      console.log(response.data);
+    });
+  });
   return <div className="App">Hello World</div>;
 }
 

a07ac90020f5f90889be994e75bd5c8643a0bcf8
Serene Aryal <serene.aryal24@gmail.com>
2023-12-10 13:48:17 -0600

    client side setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 3001;
 
 const db = require("./models");
 
+//Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
 db.sequelize.sync().then(() => {
   app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.json("Hello World!");
   });
 
   app.listen(port, () => {

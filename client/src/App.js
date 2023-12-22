03d0030a11d9130bd37b6c8c703f6953ed268b73
Serene Aryal <serene.aryal24@gmail.com>
2023-12-22 11:33:07 -0600

    tailwind setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import CreatePost from "./pages/CreatePost/CreatePost";
